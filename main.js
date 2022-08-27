d81785cb745e1554f4aa2b6f9f7f8e41fd062e18
Zhiwu Mao <zwmao@outlook.com>
2022-08-27 21:26:15 +0800

    优化代码，使用更简介的async

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const sqlite3 = require('sqlite3');
 const http = require('http')
 const Guid = require('guid');
 
-function prepareData(guid) {
-
+async function getTask(guid) {
   const copyPath = "C:/Program Files (x86)/Thunder Network/Thunder/Profiles"
   const copyDbFilePath = `${copyPath}/TaskDb.dat`
 
@@ -17,71 +16,62 @@ function prepareData(guid) {
   fs.copyFileSync(copyDbFilePath, path.resolve(__dirname, toDbFilePath))
 
   let resultList = [];
-  return new Promise((resolve) => {
-    const db = connectDB(toDbFilePath)
-    getData(db, 'SELECT TaskId,Type,UserData FROM TaskBase where Status=5')
-      .then((res) => {
-        if (res.length === 0) {
-          resolve([]);
-          return []
-        }
-        else {
-          let taskLength = res.length;
-          for (let index = 0; index < res.length; index++) {
-            let taskId = res[index]['TaskId']
 
-            let next = undefined;
-            switch (res[index]['Type']) {
-              case 1:
-                let fileId = parseFileId(res[index].UserData.toString());
-                next = getData(db, `SELECT id,name,file_extension FROM user_file where id='${fileId}'`)
-                break;
-              case 2:
-                next = getData(db, `SELECT BtFileId as id,FileName as name,'' as file_extension FROM BtFile where BtTaskId='${taskId}'`)
-                break;
-            }
+  const db = connectDB(toDbFilePath)
+  const taskList = await getRowsBySql(db, 'SELECT TaskId,Type,UserData FROM TaskBase where Status=5')
+  if (taskList.length === 0) {
+    return []
+  }
+  else {
+    let taskLength = taskList.length;
+    for (let index = 0; index < taskList.length; index++) {
+      let taskId = taskList[index]['TaskId']
+      let fileInfo = undefined;
 
-            next.then((res) => {
-              let resItem = {
-                taskId,
-                fileName: res[0].name.toString() + res[0].file_extension
-              }
+      switch (taskList[index]['Type']) {
+        case 1:
+          fileInfo = await getRowsBySql(db, `SELECT id,name,file_extension FROM user_file where id='${parseFileId(taskList[index].UserData.toString())}'`)
+          break;
+        case 2:
+          fileInfo = await getRowsBySql(db, `SELECT BtFileId as id,FileName as name,'' as file_extension FROM BtFile where BtTaskId='${taskId}'`)
+          break;
+      }
 
-              const copyTaskInfoExtTxtPath = `${copyPath}/TaskSpeedInfo/TaskInfoEx_${resItem.taskId}.txt`
-              const toTaskInfoExtTxtPath = `${toPath}/TaskInfoEx_${resItem.taskId}.txt`
+      let resItem = {
+        taskId,
+        fileName: fileInfo[0].name.toString() + fileInfo[0].file_extension
+      }
 
-              fs.copyFileSync(copyTaskInfoExtTxtPath, path.resolve(__dirname, toTaskInfoExtTxtPath))
+      const copyTaskInfoExtTxtPath = `${copyPath}/TaskSpeedInfo/TaskInfoEx_${resItem.taskId}.txt`
+      const toTaskInfoExtTxtPath = `${toPath}/TaskInfoEx_${resItem.taskId}.txt`
 
-              const data = fs.readFileSync(toTaskInfoExtTxtPath, 'utf-8')
+      fs.copyFileSync(copyTaskInfoExtTxtPath, path.resolve(__dirname, toTaskInfoExtTxtPath))
+      const data = fs.readFileSync(toTaskInfoExtTxtPath, 'utf-8')
 
-              try {
-                let downloadInfo = JSON.parse(data)
-                const progress = downloadInfo.progress
-                resItem.progress = progress
-                resItem.downloadSpeed = downloadInfo.speedInfoMap[progress].downloadSpeed / 1024
-              }
-              catch (e) {
-                console.log(e);
-                console.log(data);
-              }
+      try {
+        let { progress, speedInfoMap = {} } = JSON.parse(data)
+        let { downloadSpeed } = speedInfoMap[progress]
 
-              resultList.push(resItem);
+        resItem.progress = progress
+        resItem.downloadSpeed = downloadSpeed / 1024
+      }
+      catch (e) {
+        console.log("日志读取失败", e);
+      }
 
-              if (resultList.length === taskLength) {
-                db.close(() => {
-                  var files = fs.readdirSync(toPath)
-                  files.forEach((item) => {
-                    fs.unlinkSync(path.resolve(toPath, item))
-                  })
-                  fs.rmdirSync(toPath)
-                });
-                resolve(resultList);
-              }
-            })
-          }
-        }
+      resultList.push(resItem);
+    }
+
+    db.close(() => {
+      var files = fs.readdirSync(toPath)
+      files.forEach((item) => {
+        fs.unlinkSync(path.resolve(toPath, item))
       })
-  })
+      fs.rmdirSync(toPath)
+    });
+
+    return resultList
+  }
 }
 
 function parseFileId(text) {
@@ -96,7 +86,7 @@ function connectDB(dbFile) {
   return new sqlite3.Database(dbFile, err => { })
 }
 
-function getData(db, sql) {
+async function getRowsBySql(db, sql) {
   return new Promise((resolve, reject) => {
     db.all(sql, function (err, rows) {
       if (err) {
@@ -108,7 +98,7 @@ function getData(db, sql) {
   });
 }
 
-function Start() {
+function start() {
   let index = 0;
   var server = http.createServer()
 
@@ -118,18 +108,18 @@ function Start() {
 
   server.on('request', function (request, response) {
     console.log('已经响应了', index++);
-    prepareData(Guid.create().value)
+    getTask(Guid.create().value)
       .then((res) => {
         response.write(JSON.stringify(res))
         response.end()
       })
       .catch((res) => {
         response.write(JSON.stringify({
-          msg: err
+          msg: res
         }))
         response.end()
       })
   })
 }
 
-Start();
+start();
